563e6cd7e7b517438d52b3ec960f810c5195e531
herbertmora <56479694+herbertmora@users.noreply.github.com>
2020-04-19 11:42:31 -0500

    https://github.com/herbertmora/prueba-front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {DataSWService} from './servicios/data-sw.service';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentsMainComponent } from './contentsMain/contentsmain.component';
+import { BodyFilmsComponent } from './body-films/body-films.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentsMainComponent,
+    BodyFilmsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataSWService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
