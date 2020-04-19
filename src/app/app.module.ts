84905c31cdd8bc001289e811f7d9b6c6b2fdfee9
Mario <leyes.mario@hotmail.es>
2020-04-19 13:56:50 -0500

    Revert "https://github.com/herbertmora/prueba-front-end"
    This reverts commit 563e6cd7e7b517438d52b3ec960f810c5195e531.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {DataSWService} from './servicios/data-sw.service';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContentsMainComponent } from './contentsMain/contentsmain.component';
-import { BodyFilmsComponent } from './body-films/body-films.component';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    ContentsMainComponent,
-    BodyFilmsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [DataSWService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
